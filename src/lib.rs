4887012bc111d034a1aa2e9a5b371680e87f1bbb
David Tolnay <dtolnay@gmail.com>
2025-03-03 16:13:06 -0800

    Release 1.0.20

--- src/lib.rs
+++ src/lib.rs
@@ -175,7 +175,7 @@
 //!
 //! <br>
 
-#![doc(html_root_url = "https://docs.rs/rustversion/1.0.19")]
+#![doc(html_root_url = "https://docs.rs/rustversion/1.0.20")]
 #![allow(
     clippy::cast_lossless,
     clippy::cast_possible_truncation,
